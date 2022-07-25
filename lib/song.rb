bb9c69ddf8527c43e5c99ad50456549f9e83f1f7
Christopher Callender <christophercallender0000@gmail.com>
2022-07-25 13:32:33 -0400

    Completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -40,3 +40,14 @@ end
 
 # DB[:conn].execute("PRAGMA table_info(songs)")
 # # [[0, "id", "INTEGER", 0, nil, 1], [1, "name", "TEXT", 0, nil, 0], [2, "album", "TEXT", 0, nil, 0]]
+
+# hello = Song.new(name: "Hello", album: "25")
+# # <Song:0x00007fed21935128 @album="25", @id=nil, @name="Hello">
+# hello.save
+# # []
+# ninety_nine_problems = Song.new(name: "99 Problems", album: "The Black Album")
+# # <Song:0x00007fed218c6200 @album="The Black Album", @id=nil, @name="99 Problems">
+# ninety_nine_problems.save
+# # []
+# pry(main) > DB[:conn].execute("SELECT * FROM songs;")
+# # [[1, "Hello", "25"], [2, "99 Problems", "The Black Album"]]
